f947499577046ffc087e245613b930ae0dc14ffb
lyl3878 <lyl3878@ly.com>
2019-05-29 20:32:24 +0800

    增加文件迁移小工具-v1版本

---
+++ Tools/MoveFilesTools/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtOutPath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(122, 25);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(452, 21);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.Click += new System.EventHandler(this.txtPath_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(601, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "处理";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "输入路径：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "查找：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "替换：";
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(122, 91);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(452, 21);
+            this.txtFind.TabIndex = 0;
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(122, 124);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(452, 21);
+            this.txtReplace.TabIndex = 0;
+            // 
+            // txtOutPath
+            // 
+            this.txtOutPath.Location = new System.Drawing.Point(122, 58);
+            this.txtOutPath.Name = "txtOutPath";
+            this.txtOutPath.Size = new System.Drawing.Size(452, 21);
+            this.txtOutPath.TabIndex = 0;
+            this.txtOutPath.Click += new System.EventHandler(this.txtOutPath_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "输出路径：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOutPath);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtOutPath;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Tools/MoveFilesTools/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DirectoryInfo dir = new DirectoryInfo(txtPath.Text.Replace("\\", "/"));
+            DirectoryInfo[] DirSub = dir.GetDirectories();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (DirectoryInfo item in DirSub)
+            {
+                Console.WriteLine(item.Name);
+
+                string name = String.IsNullOrEmpty(txtFind.Text) ? item.Name : item.Name.Replace(txtFind.Text, txtReplace.Text);
+
+                string url = $"{txtOutPath.Text}\\{name}";
+                if (url != item.Name)
+                {
+                    item.MoveTo(url);
+                    stringBuilder.Append(url + Environment.NewLine);
+                }
+            }
+            MessageBox.Show(stringBuilder.ToString());
+
+
+        }
+
+        private void txtPath_Click(object sender, EventArgs e)
+        {
+            //folder控件描述Environment.SpecialFolder.Desktop;
+            folderBrowserDialog1.Description = "请选择一个文件夹：";
+            //指定folder根=桌面
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+            //是否添加新建文件夹的按钮
+            folderBrowserDialog1.ShowNewFolderButton = true;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void txtOutPath_Click(object sender, EventArgs e)
+        {
+            //folder控件描述Environment.SpecialFolder.Desktop;
+            folderBrowserDialog1.Description = "请选择一个文件夹：";
+            //指定folder根=桌面
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+            //是否添加新建文件夹的按钮
+            folderBrowserDialog1.ShowNewFolderButton = true;
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtOutPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
